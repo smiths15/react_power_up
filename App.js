6da0eaea9b90e40c985454574180d297d5dec154
Smith Saintil <smith.saintil@gmail.com>
2018-04-17 14:07:45 -0400

    added github repository to package.json and added command script for psuhing to github

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class App extends React.Component {
   render() {
     return(
-      <h2>Hello, World</h2>
+      <h2>Hello, World!</h2>
       );
   } 
 }

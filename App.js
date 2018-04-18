eb119aa598d6efd6df747f764ee73af5b2f0b84a
Smith Saintil <smith.saintil@gmail.com>
2018-04-18 15:37:24 -0400

    added prop, state, and constructor to hide or show comments using button. Also added jquery and bootstrap cdn links

--- App.js
+++ App.js
@@ -1,9 +1,94 @@
 import React from 'react';
 
-export default class App extends React.Component {
+
+
+
+// export default class App extends React.Component {
+//   render() {
+//     return(
+//       <h2>Hello World!</h2>
+//       );
+//   } 
+// }
+
+class Comment extends React.Component {
   render() {
     return(
-      <h2>Hello, World!</h2>
+      <div className = "comment">
+        <p className="comment-header">{this.props.author} </p>
+        <p className="comment-body">
+          {this.props.body}
+        </p>
+        <p className="comment-footer">
+          <a href="#" className="comment-footer-delete">
+          Delete Comment
+          </a>
+        </p>
+      </div>
       );
-  } 
+  }
+}
+
+export default class CommentBox extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {showComments: false};
+  }
+
+  render() {
+    const comments = this._getComments();
+    let commentNodes;
+    let buttonText = 'Show Comments';
+    
+    if (this.state.showComments) {
+      buttonText = 'Hide Comments';
+      commentNodes = <div className="comment-list">{comments}</div>
+    }
+
+    return(
+      <div className = "comment-box">
+        <button type="button" className="btn btn-primary btn-sm float-right" onClick={this._handleClick.bind(this)}>
+        {buttonText}
+        </button>
+        <h3>Comments</h3>
+        <h4 className="comment-count"> {this._getCommentsTitle(comments.length)}</h4>
+        {commentNodes}
+      </div>
+    );
+
+  }
+
+  _handleClick(){
+    this.setState({
+      showComments: !this.state.showComments
+    });
+  }
+
+  _getComments() {
+    const commentList = [
+      {id: 1, author: 'Morgan McCircuit', body: 'Great picture!'},
+      {id: 2, author: 'Bending Bender', body: 'Excellent stuff!'}
+    ];
+
+    return commentList.map((comment)=> {
+      return (
+        <Comment author={comment.author} body={comment.body} key={comment.id} />
+      );
+    });
+  }
+
+  _getCommentsTitle(commentCount) {
+    if (commentCount == 0) {
+      return 'No comments yet';
+    } else if (commentCount == 1) {
+      return '1 comment';
+    } else {
+      return `${commentCount} comments`;
+    }
+  }
+
+
+
+
 }
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import App from './App.js';
+import CommentBox from './App.js';
 
 
 ReactDom.render(
-  < App/>, document.getElementById('root')
+  < CommentBox/>, document.getElementById('root')
   );
